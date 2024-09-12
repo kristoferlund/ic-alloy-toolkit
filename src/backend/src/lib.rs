783d3ee02b7b1d9009c8d17800f50ae2e801d2c6
Kristofer <kristofer@fmckl.se>
2024-09-12 12:18:43 +0200

    Adding evm_rpc canister + types

--- src/backend/src/lib.rs
+++ src/backend/src/lib.rs
@@ -1,3 +1,12 @@
+#![allow(
+    dead_code,
+    non_upper_case_globals,
+    non_snake_case,
+    clippy::enum_variant_names,
+    clippy::large_enum_variant
+)]
+mod evm_rpc;
+
 #[ic_cdk::query]
 fn greet(name: String) -> String {
     format!("Hello, {}!", name)
