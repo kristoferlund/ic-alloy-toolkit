f96d6a3a24871187be8096ea8f32869f9e135866
Kristofer <kristofer@fmckl.se>
2024-09-17 11:43:59 +0200

    Adding get_balance function

--- src/backend/src/lib.rs
+++ src/backend/src/lib.rs
@@ -1,15 +1,30 @@
 use alloy::{
+    primitives::Address,
     providers::{Provider, ProviderBuilder},
     transports::icp::{EthSepoliaService, RpcService},
 };
 
 #[ic_cdk::update]
 async fn get_latest_block() -> Result<String, String> {
-    let provider = ProviderBuilder::new().on_icp(RpcService::EthSepolia(EthSepoliaService::Ankr));
+    let provider =
+        ProviderBuilder::new().on_icp(RpcService::EthSepolia(EthSepoliaService::Alchemy));
     let result = provider.get_block_number().await;
 
     match result {
         Ok(block) => Ok(block.to_string()),
+        Err(e) => Err(e.to_string()),
+    }
+}
+
+#[ic_cdk::update]
+async fn get_balance(address: String) -> Result<String, String> {
+    let address = address.parse::<Address>().map_err(|e| e.to_string())?;
+    let provider =
+        ProviderBuilder::new().on_icp(RpcService::EthSepolia(EthSepoliaService::Alchemy));
+    let result = provider.get_balance(address).await;
+
+    match result {
+        Ok(balance) => Ok(balance.to_string()),
         Err(e) => Err(e.to_string()),
     }
 }
