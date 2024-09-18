391da322c2c4d04026a0eb59037e6071acb6e9b0
Kristofer <kristofer@fmckl.se>
2024-09-18 23:03:34 +0200

    get_batch_balances

--- src/backend/src/lib.rs
+++ src/backend/src/lib.rs
@@ -35,25 +35,32 @@ async fn get_balance(address: String) -> Result<String, String> {
 }
 
 #[ic_cdk::update]
-async fn get_batch_balances(address: String) -> Result<String, String> {
-    let address = address.parse::<Address>().map_err(|e| e.to_string())?;
-
+async fn get_batch_balances(addresses: Vec<String>) -> Result<String, String> {
     let config = IcpConfig::new(RPC_SERVICE);
     let client: IcpClient = ClientBuilder::default().icp(config);
 
     let mut batch = client.new_icp_batch();
-
     let tag = BlockNumberOrTag::Latest;
+    let mut get_balance_calls = Vec::new();
 
-    let balance_fut1 = batch.add_call("eth_getBalance", &(address, tag)).unwrap();
-    let balance_fut2 = batch.add_call("eth_getBalance", &(address, tag)).unwrap();
+    for address in addresses {
+        let address = match address.parse::<Address>() {
+            Ok(addr) => addr,
+            Err(e) => return Err(e.to_string()),
+        };
+        let call = batch.add_call("eth_getBalance", &(address, tag)).unwrap();
+        get_balance_calls.push(call);
+    }
 
     batch.send().await.unwrap();
 
-    let balance1: U256 = balance_fut1.await.unwrap();
-    let balance2: U256 = balance_fut2.await.unwrap();
+    let mut balances = Vec::new();
+    for call in get_balance_calls {
+        let balance: U256 = call.await.unwrap();
+        balances.push(balance.to_string());
+    }
 
-    Ok("todo".to_string())
+    Ok(balances.join(","))
 }
 
 export_candid!();
