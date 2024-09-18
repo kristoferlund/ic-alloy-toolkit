457260e6b3dbacaa903b2f58b5e4480126cb4cb6
Kristofer <kristofer@fmckl.se>
2024-09-18 22:46:34 +0200

    Batch requests, WIP

--- src/backend/src/lib.rs
+++ src/backend/src/lib.rs
@@ -1,6 +1,8 @@
 use alloy::{
-    primitives::Address,
+    eips::BlockNumberOrTag,
+    primitives::{Address, U256},
     providers::{Provider, ProviderBuilder},
+    rpc::client::{ClientBuilder, IcpClient},
     transports::icp::{EthSepoliaService, IcpConfig, RpcService},
 };
 
@@ -29,4 +31,26 @@ async fn get_balance(address: String) -> Result<String, String> {
         Ok(balance) => Ok(balance.to_string()),
         Err(e) => Err(e.to_string()),
     }
+}
+
+#[ic_cdk::update]
+async fn get_batch_balances(address: String) -> Result<String, String> {
+    let address = address.parse::<Address>().map_err(|e| e.to_string())?;
+
+    let config = IcpConfig::new(RPC_SERVICE);
+    let client: IcpClient = ClientBuilder::default().icp(config);
+
+    let mut batch = client.new_icp_batch();
+
+    let tag = BlockNumberOrTag::Latest;
+
+    let balance_fut1 = batch.add_call("eth_getBalance", &(address, tag)).unwrap();
+    let balance_fut2 = batch.add_call("eth_getBalance", &(address, tag)).unwrap();
+
+    batch.send().await.unwrap();
+
+    let balance1: U256 = balance_fut1.await.unwrap();
+    let balance2: U256 = balance_fut2.await.unwrap();
+
+    Ok("todo".to_string())
 }
