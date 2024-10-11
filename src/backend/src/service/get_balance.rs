4001e1f428f9598394022454e51734385829347a
Kristofer <kristofer@fmckl.se>
2024-10-11 15:10:09 +0200

    Make address optional when requesting balance

--- src/backend/src/service/get_balance.rs
+++ src/backend/src/service/get_balance.rs
@@ -1,14 +1,22 @@
 use alloy::{
+    network::TxSigner,
     primitives::Address,
     providers::{Provider, ProviderBuilder},
     transports::icp::IcpConfig,
 };
 
-use crate::get_rpc_service_sepolia;
+use crate::{create_icp_sepolia_signer, get_rpc_service_sepolia};
 
 /// Request the balance of an ETH account.
 #[ic_cdk::update]
-async fn get_balance(address: String) -> Result<String, String> {
+async fn get_balance(address: Option<String>) -> Result<String, String> {
+    let address = match address {
+        Some(val) => val,
+        None => {
+            let signer = create_icp_sepolia_signer().await;
+            signer.address().to_string()
+        }
+    };
     let address = address.parse::<Address>().map_err(|e| e.to_string())?;
     let rpc_service = get_rpc_service_sepolia();
     let config = IcpConfig::new(rpc_service);
